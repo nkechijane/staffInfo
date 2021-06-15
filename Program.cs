cd2be8e48d67c37ee2c9ab20146c1b7603bd0e4a
NkechiOkeke <NkechiOkeke@LAPTOP-5QLTSC1H>
2021-06-15 07:53:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace staffInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime();
+            date = DateTime.Now;
+            employee firstEmployee = new employee(0001, "Donald Turner", date.AddYears(-2));
+            employee secondEmployee = new employee(00021, "Hillary Tina", date.AddYears(1));
+            employee thirdEmployee = new employee(0020, "Betty Ashley", date.AddYears(1));
+            employee fourthEmployee = new employee(00034, "Eddy Jones", date.AddYears(2));
+            employee fifthEmployee = new employee(00038, "Ireti Bamidele", date.AddYears(3));
+
+            firstEmployee.Department = "Accounts";
+            secondEmployee.Department = "Administration";
+            thirdEmployee.Department = "Accounts";
+            fourthEmployee.Department = "Accounts";
+            fifthEmployee.Department = "Human Resources";
+
+            firstEmployee.JobTitle = "Chief Finance Officer";
+            secondEmployee.JobTitle = "Admin Manager";
+            thirdEmployee.JobTitle = "Cashier";
+            fourthEmployee.JobTitle = "Auditor";             
+            fifthEmployee.JobTitle = "HR Manager";
+
+            //Console.WriteLine($"The first employee job title is {firstEmployee}");
+
+            employee[] employee = new employee[5] { firstEmployee, secondEmployee, thirdEmployee, fourthEmployee, fifthEmployee };
+            var a = 1;
+            Console.WriteLine("Staff Id \t Name \t\t\t Employment Date \t Department \t\t Job Title");
+            foreach (employee i in employee)
+            {
+                Console.WriteLine(i.staffId + "\t\t" + i.Name + "\t\t" + i.EmploymentDate + "\t" + i.Department + "\t\t" + i.JobTitle + "\n");
+                //Console.WriteLine($"The staffId of the employee {a} is {i.staffId}");
+                //Console.WriteLine($"The name of the employee {a} is {i.Name}");
+                //Console.WriteLine($"The employee {a} was employed on the {i.EmploymentDate}");
+                //Console.WriteLine($"The employee {a} department is {i.Department}");
+                //Console.WriteLine($"The employee {a} job title is {i.JobTitle}");
+                a++;
+            }
+
+        }
+    }
+}
---
+++ employee.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staffInfo
+{
+    class employee
+    {
+        private int _staffId;
+        private string _fullName;
+        private DateTime _employmentDate;
+        public string _department;
+        public string _jobTitle;
+
+
+        public employee(int staffId, string fullName, DateTime employmentDate)
+        {
+            _staffId = staffId;
+            _fullName = fullName;
+            _employmentDate = employmentDate;
+        }
+        public int staffId
+        {
+            get
+            {
+                return _staffId;
+            }
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _fullName;
+            }
+        }
+
+
+        public string JobTitle
+        {       
+
+            get
+            {
+                return _jobTitle;
+            }
+
+            set
+            {
+                _jobTitle = value;
+            }
+
+        }
+
+        public DateTime EmploymentDate
+        {
+            get
+            {
+                return _employmentDate;
+            }
+        }
+
+        public string Department 
+        {
+            get
+            {
+                return _department;
+            }
+
+            set 
+            {
+                _department = value;
+            }
+        }
+
+        public void ClockIn()
+        {
+            Console.Write("I am clocking into the office");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("i am busy at work");
+        }
+
+        public void ClockOut()
+        {
+            Console.WriteLine("i am done for the day, therefore am clocking out");
+        }
+        
+        
+    }
+}
+
+
+    
